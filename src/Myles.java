aba468333dd03bc1d506fc84ac10e6ee70edc183
Anastasia Baturina <89099934219@mail.ru>
2020-11-29 21:04:53 +0300

    initial commit

---
+++ src/Myles.java
@@ -0,0 +1,9 @@
+public class Myles {
+    public static void main(String[] args) {
+        int priceMile = 20;
+        int ticket = 900;
+        int bonusSize = ticket / priceMile;
+        System.out.println(bonusSize);
+        ;
+    }
+}
